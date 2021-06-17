e24738f699fd19bdbfb85c38de4f653315007b44
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-06-17 08:36:05 -0300

    Exercise 1 higherNumber

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter number b: ");
+        int b = sc.nextInt();
+
+        int higher = higherNumber(a, b);
+
+        System.out.println(higher);
+        sc.close();
+    }
+
+    public static int higherNumber(int a, int b) {
+        if(a > b) {
+            return 1;
+        } else if (a == b) {
+            return 0;
+        } else {
+            return -1;
+        }
     }
 }
