dbd5a7c8b6739cee4dade2b19cc30c9fba567e86
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-06-18 16:36:25 -0300

    Exercise Time

---
+++ src/studyTime/ExampleTime.java
@@ -0,0 +1,18 @@
+package studyTime;
+
+import java.sql.Time;
+import java.time.Instant;
+import java.time.LocalTime;
+
+
+public class ExampleTime {
+
+    public static void main(String[] args) {
+
+        Time sqlTime1  = Time.valueOf("07:00:00");
+        System.out.println(sqlTime1);
+
+
+    }
+}
+
