9578577a8931b60d8315c3d787d5eb5ff98e9324
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-07-19 11:55:50 -0300

    exercise9 list

---
+++ src/exercise9/Program.java
@@ -0,0 +1,40 @@
+package exercise9;
+
+import exercise9.entity.Person;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // i - crie uma classe chamada Pessoa que contém dois campos: um id (int) e um nome (String).
+        // Depois, crie um método (na classe Programa/Main) que recebe um número (int) chamado 'N'  e devolve uma lista de pessoas.
+        // O método deve criar e devolver N pessoas (quantidade que veio no tamanho). O id de cada pessoa deve ir de 0 até N-1;
+        // o nome de cada pessoa deve ser "Gandalf" com o id da pessoa concatenado no nome (ex.: pessoa com id 5, o nome deve ser "Gandalf5")
+
+        int n = 5;
+
+        List<Person> resultList = getPerson(n);
+
+        for (int i = 0; i < resultList.size(); i++) {
+            Person person = resultList.get(i);
+            System.out.println(person.getNameId());
+            System.out.println(person.getName());
+        }
+    }
+
+    public static List<Person> getPerson(int n){
+        List<Person> resultList = new ArrayList<>();
+
+        for (int i = 0; i < n; i ++) {
+            int id = i;
+            String nameId = "Gandalf" + id ;
+            String name = "Gandalf" ;
+            Person person = new Person(nameId, name, id);
+            resultList.add(person);
+        }
+        return resultList;
+    }
+
+}
---
+++ src/exercise9/entity/Person.java
@@ -0,0 +1,53 @@
+package exercise9.entity;
+
+public class Person {
+
+    private String name;
+    private int id;
+    private String nameId;
+
+    public Person() {
+    }
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Person(String nameId, String name, int id) {
+        this.nameId = nameId;
+        this.name = name;
+        this.id = id;
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameId() {
+        return nameId;
+    }
+
+    public void setNameId(String nameId) {
+        this.nameId = nameId;
+    }
+
+    @Override
+    public String toString() {
+        return name + '\n' +
+               nameId
+                ;
+    }
+}
