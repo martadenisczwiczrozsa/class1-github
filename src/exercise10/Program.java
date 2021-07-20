b971f0aa717796a7cc85667ef972c8c3cf47966c
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-07-20 14:49:33 -0300

    exercise 11 array

--- src/exercise10/Program.java
+++ src/exercise10/Program.java
@@ -7,9 +7,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        // j - crie um método que recebe um número N e retorna um array de números.
+        // j - crie um método que recebe um número N e retorna um arrayList de números.
         // O método deve criar o array com o tamanho do número recebido e preencher o array com os valores de 0 até N-1.
-        // Ex.: se N for igual a 5, o método deve retornar o array [0, 1, 2, 3, 4].
+        // Ex.: se N for igual a 5, o método deve retornar o arrayList [0, 1, 2, 3, 4].
         // Também imprima cada valor do array retornado em uma linha
 
         int n = 5;
---
+++ src/exercise11/Program.java
@@ -0,0 +1,29 @@
+package exercise11;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // k - crie um método que recebe um número N e retorna um array de números.
+        // O método deve criar o array com o tamanho do número recebido e preencher o array com os valores de 0 até N-1.
+        // Ex.: se N for igual a 5, o método deve retornar o array [0, 1, 2, 3, 4].
+        // Também imprima cada valor do array retornado em uma linha
+
+        int number = 5;
+
+        int[] numbers = arrayNumbers(number);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public static int[] arrayNumbers (int number) {
+
+        int[] arrayNumber = new int[number];
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+            arrayNumber[i] = i;
+        }
+        return arrayNumber;
+    }
+}
