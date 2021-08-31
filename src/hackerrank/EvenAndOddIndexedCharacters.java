be67335a9cdc662832c1cffc1fa7784a084554ae
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-08-31 19:10:28 -0300

    Add new exercises

---
+++ src/hackerrank/EvenAndOddIndexedCharacters.java
@@ -0,0 +1,41 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.joining;
+
+public class EvenAndOddIndexedCharacters {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of test cases: ");
+        int T = sc.nextInt();
+        sc.nextLine();
+
+
+        List<String> even = new ArrayList<>();
+        List<String> odd = new ArrayList<>();
+
+        for(int i = 0; i < T; i++) {
+            System.out.print("Enter a string: ");
+            String S = sc.nextLine();
+
+            for (int j = 0; j < S.length(); j++) {
+                if (j % 2 == 0) {
+                    even.add(S.charAt(j) + "");
+                } else {
+                    odd.add(S.charAt(j) + "");
+                }
+            }
+            System.out.print(even.stream().collect(joining("")) + " " + odd.stream().collect(joining(""))+ "\n");
+            even.clear();
+            odd.clear();
+        }
+
+        sc.close();
+    }
+}
---
+++ src/hackerrank/PersonInitialAge.java
@@ -0,0 +1,52 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class PersonInitialAge {
+
+    private int age;
+
+    public PersonInitialAge(int initialAge) {
+        if(initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.age = 0;
+        } else {
+            this.age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        String result;
+        if (age < 13) {
+            result= "You are young.";
+        } else  if (age >= 13 && age < 18){
+            result = "You are a teenager.";
+        } else {
+            result = "You are old.";
+        }
+        System.out.println(result);
+
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        this.age++;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            PersonInitialAge p = new PersonInitialAge(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
