dd4f221e82b11020aa471295d2ffed37140c52a6
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-09-01 08:22:19 -0300

    Add new exercise

--- src/hackerrank/ArrayNumbers.java
+++ src/hackerrank/ArrayNumbers.java
@@ -1,2 +1,29 @@
-package hackerrank;public class ArrayNumbers {
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class ArrayNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        bufferedReader.close();
+
+        Collections.reverse(arr);
+        for (int i = 0; i < arr.size(); i++){
+            System.out.print(arr.get(i) + " ");
+        }
+    }
 }
