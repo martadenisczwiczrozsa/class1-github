138438d21e3118fbf48ae0baa7d9340c01c7c337
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-09-02 14:40:54 -0300

    Add new exercise

--- src/hackerrank/DiagonalDifference.java
+++ src/hackerrank/DiagonalDifference.java
@@ -1,7 +1,5 @@
 package hackerrank;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class DiagonalDifference {
--- src/hackerrank/PrintStaircase.java
+++ src/hackerrank/PrintStaircase.java
@@ -1,2 +1,27 @@
-package hackerrank;public class PrintStaircase {
+package hackerrank;
+
+import java.util.Scanner;
+
+public class PrintStaircase {
+    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       System.out.print("What is the size of the staircase? ");
+       int n = sc.nextInt();
+
+
+       for (int i = 0 ; i < n; i++) {
+           for (int j = 1; j <= n; j++) {
+               int zeroCount = n - i - 1;
+               if (j <= zeroCount) {
+                   System.out.print(" ");
+               } else {
+                   System.out.print("#");
+               }
+           }
+           System.out.println();
+       }
+       sc.close();
+    }
+
 }
