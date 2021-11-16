919fb091530225753881277f0d813cd7effb667d
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-11-16 17:05:29 -0300

    Add new exercises

---
+++ src/testExercises/HighestPalindromicNumber.java
@@ -0,0 +1,10 @@
+package testExercises;
+
+// Faça um programa que calcule o maior número palindrômico feito a partir do produto de dois números de 3 dígitos.
+// Ex: O maior palindrômico feito a partir do produto de dois números de dois dígitos e 9009 = 91*99.
+
+public class HighestPalindromicNumber {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/testExercises/RandomNumber.java
@@ -0,0 +1,11 @@
+package testExercises;
+
+//Faça um programa que gera um número aleatório de 1 a 1000. O usuário deve tentar acertar qual o número foi gerado,
+// a cada tentativa o programa deverá informar se o chute e menor ou maior que o número gerado.
+// O programa acaba quando o usuário acerta o número gerado. O programa deve informar em quantas tentativas o número foi descoberto.
+
+public class RandomNumber {
+    public static void main(String[] args) {
+
+    }
+}
