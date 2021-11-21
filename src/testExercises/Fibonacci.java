bbbf06099a58043993cca13d4d77d1a3e6eb7133
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-11-21 08:21:10 -0300

    Add new exercise

--- src/testExercises/Fibonacci.java
+++ src/testExercises/Fibonacci.java
@@ -1,16 +1,32 @@
 package testExercises;
 
-/* A sequência de Fibonacci é uma sucessão de números obtidos pela soma dos anteriores.
-0, se n 0
-1, se n 0
+/*Cada termo da sequencia Fibonacci é gerado pela soma dos dois últimos termos.
+Se começarmos a sequencia pelos números 1 e 2, os 10 primeiros termos da sequencia serão:
+        1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
-Fn = F(n -1) + F(n - 2)
-
-Resultante em 0, 1, 1, 2, 3, 5, 8, 13, ...
-Escreva um algoritmo para gerar automaticamente a sequência de Fibonacci até o vigésimo termo.*/
+Então, considerando a sequencia de fibonacci começando pelos termos 1 e 2, até que o valor do último termo não exceda de 4 milhões.
+Encontre a somatória de todos os termos que são PARES. */
 
 public class Fibonacci {
     public static void main(String[] args) {
 
+        int previousNumber = 1;
+        int nextNumber = 2;
+        int newNumber = previousNumber + nextNumber;
+
+        int sum = 0;
+
+        while (newNumber <= 4000000) {
+            previousNumber = nextNumber;
+            nextNumber = newNumber;
+
+            if (nextNumber % 2 == 0) {
+                sum += nextNumber;
+            }
+
+            newNumber = previousNumber + nextNumber;
+        }
+
+        System.out.println(sum);
     }
 }
